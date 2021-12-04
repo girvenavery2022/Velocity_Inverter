53ab6c6a0c889b24ee5861adeb06bbed7bf34a0b
Avery <agirven@umich.edu>
2021-12-04 15:38:14 -0500

    first commit

---
+++ src/vel_processor.cpp
@@ -0,0 +1,34 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include <geometry_msgs/msg/twist.hpp>
+using std::placeholders::_1;
+
+class VelInverter : public rclcpp::Node
+{
+  public:
+    VelInverter()
+    : Node("velocity_inverter")
+    {
+      subscription_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      "/cmd_vel", 10, std::bind(&VelInverter::topic_callback, this, _1));  
+      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/mammoth/cmd_vel", 10);
+    }
+
+  private:
+    void topic_callback(const geometry_msgs::msg::Twist::SharedPtr msg) const
+    {
+      msg->linear.x = msg->linear.x * -1;
+      publisher_->publish(*msg);
+    }
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<VelInverter>());
+  rclcpp::shutdown();
+  return 0;
+}
